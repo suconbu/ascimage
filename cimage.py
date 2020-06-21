c675a3f76226af65df30fc3972d4ff48144b59dd
miwa <suconbu@gmail.com>
2020-06-21 15:30:19 +0900

    Refactoring: Add Image class in imageutil

--- cimage.py
+++ cimage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import ctypes
-from imageutil import imageutil
+from imageutil import Image
 from argparse import ArgumentParser
 
 DEFAULT_WIDTH = 80
@@ -12,28 +12,29 @@ DEFAULT_TONE = " .,:;i1tfLCG08@"
 def get_brightness(r, g, b):
     return r * 0.299 + g * 0.587 + b * 0.114  # ITU-R Rec. BT.601
 
-def output_image(args, data, value_max, width, height, channels):
-    assert(channels == 3 or channels == 4)
+def output_image(args, image):
+    assert(image.channels == 3 or image.channels == 4)
 
     tone = args.tone
     index = 0
+    vmin, vmax = image.value_range
     if args.frame:
-        print("-" * (width + 2))
-    for y in range(0, height):
+        print("-" * (image.width + 2))
+    for y in range(0, image.height):
         row = ""
-        for x in range(0, width):
-            a = max(0, min(data[index + 3], value_max)) / value_max
-            r = max(0, min(data[index + 0], value_max)) / value_max * a
-            g = max(0, min(data[index + 1], value_max)) / value_max * a
-            b = max(0, min(data[index + 2], value_max)) / value_max * a
+        for x in range(0, image.width):
+            a = max(vmin, min(image.data[index + 3], vmax)) / vmax if (image.channels == 4) else 1
+            r = max(vmin, min(image.data[index + 0], vmax)) / vmax * a
+            g = max(vmin, min(image.data[index + 1], vmax)) / vmax * a
+            b = max(vmin, min(image.data[index + 2], vmax)) / vmax * a
             brightness = get_brightness(r, g, b)
             row += tone[min(int(brightness * len(tone)), len(tone) - 1)]
-            index += channels
+            index += image.channels
         if args.frame:
             row = "-" + row + "-"
         print(row)
     if args.frame:
-        print("-" * (width + 2))
+        print("-" * (image.width + 2))
 
 def main(argv):
     ap = ArgumentParser(add_help=False)
@@ -45,41 +46,29 @@ def main(argv):
     ap.add_argument("-i", "--invert", action="store_true")
     args = ap.parse_args(argv[1:])
 
-    if args.invert:
-        args.tone = args.tone[::-1]
-
     if not os.path.exists(args.filename):
         print(f"{args.filename}: No such file", file=sys.stderr)
         return 1
 
-    c_filename = args.filename.encode("utf-8")
-    c_width = ctypes.c_int32()
-    c_height = ctypes.c_int32()
-    c_data = ctypes.POINTER(ctypes.c_uint8)
-    channels = 4
+    if args.invert:
+        args.tone = args.tone[::-1]
 
-    c_data = imageutil.load_image(c_filename, c_width, c_height, channels)
+    image = Image.load(args.filename, channels=4)
 
     resized_width = max(0, int(args.width))
     resized_height = max(0, int(args.height))
     if resized_width == 0 and resized_height == 0:
-        resized_width = min(c_width.value, DEFAULT_WIDTH)
+        resized_width = min(image.width, DEFAULT_WIDTH)
 
     if resized_width == 0:
-        resized_width = max(1, int(resized_height * c_width.value / (c_height.value / 2)))
+        resized_width = max(1, int((resized_height * image.width / image.height / 2)))
     elif resized_height == 0:
-        resized_height = max(1, int(resized_width * (c_height.value / 2) / c_width.value))
+        resized_height = max(1, int((resized_width * image.height / 2) / image.width))
     else:
         pass
 
-    c_resized_data = (ctypes.c_uint8 * (channels * resized_width * resized_height))()
-    imageutil.resize_image(
-        c_data, c_width.value, c_height.value, 0,
-        c_resized_data, resized_width, resized_height, 0,
-        channels)
-    imageutil.free_image(c_data)
-
-    output_image(args, c_resized_data, 255, resized_width, resized_height, channels)
+    resized_image = image.get_resized(resized_width, resized_height)
+    output_image(args, resized_image)
 
     return 0
 
--- imageutil.py
+++ imageutil.py
@@ -8,10 +8,6 @@ imageutil.load_image.restype = ctypes.POINTER(ctypes.c_uint8)
 imageutil.load_image.argtypes = (
     ctypes.c_char_p, ctypes.POINTER(ctypes.c_int32), ctypes.POINTER(ctypes.c_int32), ctypes.c_int32)
 
-imageutil.load_imagef.restype = ctypes.POINTER(ctypes.c_float)
-imageutil.load_imagef.argtypes = (
-    ctypes.c_char_p, ctypes.POINTER(ctypes.c_int32), ctypes.POINTER(ctypes.c_int32), ctypes.c_int32)
-
 imageutil.free_image.restype = None
 imageutil.free_image.argtypes = (ctypes.c_void_p,)
 
@@ -21,8 +17,45 @@ imageutil.resize_image.argtypes = (
     ctypes.POINTER(ctypes.c_uint8), ctypes.c_int32, ctypes.c_int32, ctypes.c_int32,
     ctypes.c_int32)
 
-imageutil.resize_imagef.restype = ctypes.c_int32
-imageutil.resize_imagef.argtypes = (
-    ctypes.POINTER(ctypes.c_float), ctypes.c_int32, ctypes.c_int32, ctypes.c_int32,
-    ctypes.POINTER(ctypes.c_float), ctypes.c_int32, ctypes.c_int32, ctypes.c_int32,
-    ctypes.c_int32)
+class Image(object):
+    def __init__(self):
+        self.data = None
+        self.width = 0
+        self.height = 0
+        self.channels = 0
+        self.value_range = (0, 0)
+
+    @classmethod
+    def load(cls, filename, channels):
+        image = Image()
+        c_filename = filename.encode("utf-8")
+        c_width = ctypes.c_int32()
+        c_height = ctypes.c_int32()
+        data = imageutil.load_image(c_filename, c_width, c_height, channels)
+        data_count = int(channels * c_width.value * c_height.value)
+        image.data = (ctypes.c_uint8 * data_count)()
+        ctypes.memmove(image.data, data, data_count)
+        imageutil.free_image(data)
+        image.width = c_width.value
+        image.height = c_height.value
+        image.channels = channels
+        image.value_range = (0, 255)
+        return image
+
+    def get_resized(self, resized_width, resized_height, resized_channels=None):
+        if not self.data:
+            return None
+        resized_channels = resized_channels or self.channels
+        resized_image = Image()
+        resized_image.width = int(resized_width)
+        resized_image.height = int(resized_height)
+        resized_image.channels = int(resized_channels)
+        data_count = int(resized_image.channels * resized_image.width * resized_image.height)
+        resized_image.data = (ctypes.c_uint8 * data_count)()
+        resized_image.value_range = self.value_range
+        result = imageutil.resize_image(
+            self.data, self.width, self.height, 0,
+            resized_image.data, resized_image.width, resized_image.height, 0,
+            resized_image.channels
+            )
+        return resized_image if (result == 1) else None
